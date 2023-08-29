c01cf8c1a58e9837336c06bc6250d26427992287
ductung <ductung@bfast.dev>
2023-08-29 09:39:21 +0700

    Config watch for iOS

---
+++ ios/watchos Watch App/watchosApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchosApp.swift
+//  watchos Watch App
+//
+//  Created by Vu Duc Tung  on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct watchos_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
